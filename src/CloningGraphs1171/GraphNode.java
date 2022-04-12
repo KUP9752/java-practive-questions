b8e838367f27ebd12b2abb0880da273230d5aa8f
kup21 <kup21@ic.ac.uk>
2022-04-12 11:24:02 +0100

    kup21 : --STARTED-- 1171: Cloning Graphs

---
+++ src/CloningGraphs1171/GraphNode.java
@@ -0,0 +1,34 @@
+package CloningGraphs1171;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GraphNode<E> {
+
+    private E key;
+    private final List<GraphNode<E>> successors;
+
+    public GraphNode() {
+        successors = new ArrayList<>();
+    }
+
+    public int getNumberOfSuccessors() {
+        return successors.size();
+    }
+
+    public GraphNode<E> getSuccessor(int successorIndex) {
+        return successors.get(successorIndex);
+    }
+
+    public void addSuccessor(GraphNode<E> successor) {
+        successors.add(successor);
+    }
+
+    public E getKey() {
+        return key;
+    }
+
+    public void setKey(E key) {
+        this.key = key;
+    }
+
+}
