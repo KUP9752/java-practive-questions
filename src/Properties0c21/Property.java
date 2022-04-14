ed4f822192a5820e4efcb70376833c666f9e9aab
kup21 <kup21@ic.ac.uk>
2022-04-14 10:34:38 +0100

    kup21 : --DONE-- 236b: Extended Properties0c21

--- src/Properties0c21/Property.java
+++ src/Properties0c21/Property.java
@@ -1,4 +1,8 @@
 package Properties0c21;
 
 public class Property {
+    protected int noBedrooms;
+    protected int noReceptions;
+    protected int noBathrooms;
+    protected double sqFootArea;
 }
